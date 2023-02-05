a8476a4e53952546c0d88c00925dcc1d8f56161f
phucnguyen <phucnguyen5102003@gmail.com>
2023-02-05 23:50:21 +0700

    add app.css

--- src/router/siteRouter.js
+++ src/router/siteRouter.js
@@ -3,6 +3,7 @@ const siteRouter = express.Router();
 
 const siteController = require('../app/controller/siteController');
 
+siteRouter.get('/css/app.css', siteController.css);
 siteRouter.get('/search', siteController.search);
 siteRouter.get('/', siteController.home);
 
