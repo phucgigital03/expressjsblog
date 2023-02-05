0c59f285401f3987a8d0b4dc86b02ba316508e36
phucnguyen <phucnguyen5102003@gmail.com>
2023-02-06 00:40:04 +0700

    remove router app.css

--- src/app/controller/siteController.js
+++ src/app/controller/siteController.js
@@ -1,16 +1,9 @@
 const mongooseObj = require('../../util/mongoose')
-const path = require('path')
-console.log()
+
 class siteController{
     // [get]: /
     home(req,res){
         res.json('home')
-    }
-
-    // [get]: /css/app.css
-    css(req,res){
-        res.status(201).sendFile(path.join(__dirname,'public','css','app.css'))
-        return;
     }
 
     // [get]: /search
--- src/router/siteRouter.js
+++ src/router/siteRouter.js
@@ -3,7 +3,6 @@ const siteRouter = express.Router();
 
 const siteController = require('../app/controller/siteController');
 
-siteRouter.get('/css/app.css', siteController.css);
 siteRouter.get('/search', siteController.search);
 siteRouter.get('/', siteController.home);
 
