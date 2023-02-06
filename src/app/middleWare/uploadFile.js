e47125b208340e742b78d290a15780efc45ef8f1
phucnguyen <phucnguyen5102003@gmail.com>
2023-02-07 03:18:57 +0700

    change code fix opendir of server

--- src/app/middleWare/uploadFile.js
+++ src/app/middleWare/uploadFile.js
@@ -1,26 +1,9 @@
 const path = require('path');
 const multer = require('multer')
 
-let dir;
-const createDir = ()=>{
-    let index;
-    const eleDir = __dirname.split('/');
-    eleDir.find((ele,ind) => {
-        index = ind + 1;
-        return ele === 'src'
-    })
-    eleDir.splice(index,2);
-    const rootDir = eleDir.reduce((stringDir,ele)=>{
-        stringDir += `/${ele}`;
-        return stringDir
-    })
-    dir = path.join(rootDir,'public','img')
-}
-createDir()
-
 const storage = multer.diskStorage({
     destination(req,file,cb){
-        cb(null,dir)
+        cb(null,'/public/img/')
     },
     filename(req,file,cb){
         const ext = path.extname(file.originalname)
